b14a4b89322b95c81cc434f6a2b47b005f92aeff
SamanDev <mac.developers7@gmail.com>
2025-01-30 14:58:15 +0330

    fff

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ if (pathArr.length == 3) {
 //console.log(_auth);
 
 //const WEB_URL = process.env.REACT_APP_MODE === "production" ? `wss://${process.env.REACT_APP_DOMAIN_NAME}/` : `ws://${loc.hostname}:8080`;
-const WEB_URL = `wss://mbaccarat.royale777.vip/`;
+const WEB_URL = `wss://mbaccarat.usdtpoker.club/`;
 // (A) LOCK SCREEN ORIENTATION
 const betAreas = [
     { x: "PLAYER", sidep: 2, side3: 10 },
